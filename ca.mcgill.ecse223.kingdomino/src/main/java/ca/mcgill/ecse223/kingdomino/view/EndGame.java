4ae115994840c70fd09c09f60aa552d79cf0cdad
KeonOlszewski <olszkeon@gmail.com>
2020-04-09 15:32:55 -0400

    improved EndGame

--- ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/view/EndGame.java
+++ ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/view/EndGame.java
@@ -16,8 +16,18 @@ import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 import javax.swing.border.Border;
 import javax.swing.GroupLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.WindowConstants;
 
 public class EndGame extends JFrame{
+	
+	 private JPanel jContentPane = null;
+
 	
 	public static void main(String[] args) {
 		
@@ -35,30 +45,51 @@ public class EndGame extends JFrame{
 		this.setSize(500,400);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setTitle("Kingdomino");
+		this.setContentPane(getJContentPane());
+	        
 		
-		JLabel winner = new JLabel("WINNER!");
-		JLabel player = new JLabel("player goes here");
+	
 		
-		GroupLayout layout = new GroupLayout(getContentPane());
-		getContentPane().setLayout(layout);
-		layout.setAutoCreateGaps(true);
-		layout.setAutoCreateContainerGaps(true);
-		layout.setHorizontalGroup(
-				layout.createSequentialGroup()
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
-						.addComponent(player)
-						.addComponent(winner)
-						)
-				);
-		
-		layout.setVerticalGroup(
-				layout.createParallelGroup(GroupLayout.Alignment.CENTER)
-				.addGroup(layout.createSequentialGroup()
-					.addComponent(player)
-					.addComponent(winner)
-					)
-				);
-						
-	}
+//		GroupLayout layout = new GroupLayout(getContentPane());
+//		getContentPane().setLayout(layout);
+//		layout.setAutoCreateGaps(true);
+//		layout.setAutoCreateContainerGaps(true);
+//		layout.setHorizontalGroup(
+//				layout.createSequentialGroup()
+//				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
+//						.addComponent(player)
+//						.addComponent(winner)
+//						)
+//				);
+//		
+//		layout.setVerticalGroup(
+//				layout.createParallelGroup(GroupLayout.Alignment.CENTER)
+//				.addGroup(layout.createSequentialGroup()
+//					.addComponent(player)
+//					.addComponent(winner)
+//					)
+//				);
+//						
+//	}
 
+	}	
+	
+	 private JPanel getJContentPane() {
+	        if (jContentPane == null) {
+	            jContentPane = new JPanel();
+	            jContentPane.setLayout(null);
+	            JPanel panel = new JPanel();
+	            panel.setBounds((this.getWidth()/2)-60, (this.getHeight()/2)-30, 120, 35);
+	            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+	            jContentPane.add(panel);
+	            panel.setBackground(Color.WHITE);
+
+	            JLabel winner = new JLabel("WINNER!");
+	    		JLabel player = new JLabel("player goes here");
+	    		
+	    		panel.add(winner);
+	    		panel.add(player);
+	        }
+	        return jContentPane;
+	    }
 }
