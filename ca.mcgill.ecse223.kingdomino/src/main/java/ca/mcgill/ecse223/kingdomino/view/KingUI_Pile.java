0e310607a41bbd50e23e3d2cfd14652cdb0fc412
Guan-Eric <60152251+Guan-Eric@users.noreply.github.com>
2020-04-16 17:37:21 -0400

    Update KingUI_Pile.java

--- ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/view/KingUI_Pile.java
+++ ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/view/KingUI_Pile.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
@@ -11,6 +12,10 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 import javax.swing.border.Border;
+
+import ca.mcgill.ecse223.kingdomino.controller.KDController;
+import ca.mcgill.ecse223.kingdomino.model.Domino;
+import ca.mcgill.ecse223.kingdomino.model.TerrainType;
 
 @SuppressWarnings("serial")
 public class KingUI_Pile extends JFrame {
@@ -42,23 +47,23 @@ public class KingUI_Pile extends JFrame {
 		JPanel dominos = new JPanel();
 		JPanel domino[] = new JPanel[48];
 		JPanel tile[] = new JPanel[96];
-		
+		List<Domino> dom = KDController.browseDominos();
 		for (int i = 0; i < 48; i++) {
 			
 			if (dominoInPile) {
 				
 				tile[i] = new JPanel();
-				tile[i].setBackground(Color.LIGHT_GRAY);
+				tile[i].setBackground(getTerrainColor(dom.get(i).getLeftTile()));
 				tile[i].setBorder(border);
-				if (tileHasCrown) {
+				for (int j = 0; j < dom.get(i).getLeftCrown(); j++) {
 					JPanel crown = new JPanel();
 					crown.setBackground(Color.yellow);
-					tile[i].add(crown);
+					tile[i+48].add(crown);
 				}
 				tile[i+48] = new JPanel();
-				tile[i+48].setBackground(Color.GRAY);
+				tile[i+48].setBackground(getTerrainColor(dom.get(i).getRightTile()));
 				tile[i+48].setBorder(border);
-				if (tileHasCrown) {
+				for (int j = 0; j < dom.get(i).getRightCrown(); j++) {
 					JPanel crown = new JPanel();
 					crown.setBackground(Color.yellow);
 					tile[i+48].add(crown);
@@ -69,12 +74,29 @@ public class KingUI_Pile extends JFrame {
 				dominos.add(domino[i]);
 			}
 		}
-		
-		
-		
+
 		p.add(pile);
 		frame.add(p, BorderLayout.NORTH);
 		frame.add(dominos, BorderLayout.CENTER);
 		frame.setVisible(true);
+	}
+	
+	public static Color getTerrainColor(TerrainType t) {
+		switch (t) {
+		case WheatField:
+			return Color.orange;
+		case Forest:
+			return Color.green;
+		case Mountain:
+			return Color.magenta;
+		case Grass:
+			return Color.DARK_GRAY;
+		case Swamp:
+			return Color.cyan;
+		case Lake:
+			return Color.blue;
+		default:
+			throw new java.lang.IllegalArgumentException("Invalid terrain type: " + t);
+		}
 	}
 }
