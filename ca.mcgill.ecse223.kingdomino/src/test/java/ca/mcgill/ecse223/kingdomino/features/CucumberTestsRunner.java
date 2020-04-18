b0f3c44999ae7de084f668eaf3cc44c55c03279d
jhanmtl <jing.han@mail.mcgill.ca>
2020-04-17 22:41:30 -0400

    really done!

--- ca.mcgill.ecse223.kingdomino/src/test/java/ca/mcgill/ecse223/kingdomino/features/CucumberTestsRunner.java
+++ ca.mcgill.ecse223.kingdomino/src/test/java/ca/mcgill/ecse223/kingdomino/features/CucumberTestsRunner.java
@@ -8,50 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
 		plugin = "pretty", 
 		features = "src/test/resources/ca/mcgill/ecse223/kingdomino/features",
-//		features = "src/test/resources/ca/mcgill/ecse223/kingdomino/features/CreateNextDraft.feature",
-//		features = {
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/SaveGame.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/BrowseDominoPile.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/CalculateBonusScores.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/CalculatePlayerScore.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/CalculatePropertyAttributes.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/CalculateRanking.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/CalculatingPlayerScore.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/ChooseNextDomino.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/CreateNextDraft.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/DiscardDomino.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/DiscardingDomino.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/DiscardingLastDomino.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/IdentifyProperties.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/InitializingGame.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/MoveCurrentDomino.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/OrderAndRevealNextDraft.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/PlaceDomino.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/PlacingDomino.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/PlacingLastDomino.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/ProvideUserProfile.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/ResolveTiebreak.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/RotateCurrentDomino.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/SelectingDomino.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/SelectingFirstDomino.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/SetGameOptions.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/ShuffleDominos.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/SortingAndRevealingDraft.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/StartANewGame.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/VerifyCastleAdjacency.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/VerifyGridSize.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/VerifyNeighborAdjacency.feature",
-//					"src/test/resources/ca/mcgill/ecse223/kingdomino/features/VerifyNoOverlapping.feature"
-//					},
-		
 		glue = "ca.mcgill.ecse223.kingdomino.features")
 public class CucumberTestsRunner {
 }
-//"src/test/resources/ca/mcgill/ecse223/kingdomino/features/CalculateRanking.feature",
-//"src/test/resources/ca/mcgill/ecse223/kingdomino/features/CalculatingPlayerScore.feature",
-//"src/test/resources/ca/mcgill/ecse223/kingdomino/features/DiscardingDomino.feature",
-//"src/test/resources/ca/mcgill/ecse223/kingdomino/features/DiscardingLastDomino.feature",
-
-
 
 
